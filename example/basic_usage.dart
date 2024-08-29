543c5c0c6b9a40fc557f544fbba6e79539278569
Aditya Kantipudi <akantipudi@loginsoft.com>
2024-08-29 12:17:39 +0530

    Add more directives to the example

--- example/basic_usage.dart
+++ example/basic_usage.dart
@@ -1,12 +1,23 @@
-import '../lib/imageengine_helpers_dart.dart';
+import 'package:imageengine_helpers_dart/imageengine_helpers_dart.dart';
 
 void main() {
   // Create an instance of IEDirectives with some example values
   IEDirectives directives = IEDirectives(
     width: 800,
     height: 600,
-    crop: 'fit',
+    autoWidthFallback: 1000,
+    scaleToScreenWidth: 100,
+    crop: '200,300,0,0',
     outputFormat: IEFormat.jpg,
+    fitMethod: IEFit.box,
+    compression: 75,
+    sharpness: 10,
+    rotate: 90,
+    inline: true,
+    keepMeta: false,
+    noOptimization: false,
+    forceDownload: false,
+    maxDevicePixelRatio: 2.0,
   );
 
   // Source image URL
