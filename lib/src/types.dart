3f132c61bc1a9aef58e42a6ff79ac2fc7ce26838
Aditya Kantipudi <akantipudi@loginsoft.com>
2024-09-04 16:33:42 +0530

    Add toMap() and fromMap() methods to IEDirectives
    - Implement toMap() for serialization
    - Add factory constructor fromMap() for deserialization
    - Enhance type safety and data conversion for IEDirectives

--- lib/src/types.dart
+++ lib/src/types.dart
@@ -61,4 +61,64 @@ class IEDirectives {
     this.forceDownload,
     this.maxDevicePixelRatio,
   });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'width': width,
+      'height': height,
+      'autoWidthFallback': autoWidthFallback,
+      'scaleToScreenWidth': scaleToScreenWidth,
+      'crop': crop,
+      'outputFormat': outputFormat?.toString().split('.').last,
+      'format': format?.toString().split('.').last,
+      'fitMethod': fitMethod?.toString().split('.').last,
+      'fit': fit?.toString().split('.').last,
+      'compression': compression,
+      'sharpness': sharpness,
+      'rotate': rotate,
+      'inline': inline,
+      'keepMeta': keepMeta,
+      'noOptimization': noOptimization,
+      'forceDownload': forceDownload,
+      'maxDevicePixelRatio': maxDevicePixelRatio,
+    };
+  }
+
+  factory IEDirectives.fromMap(Map<String, dynamic> map) {
+    return IEDirectives(
+      width: map['width'] as int?,
+      height: map['height'] as int?,
+      autoWidthFallback: map['autoWidthFallback'] as int?,
+      scaleToScreenWidth: map['scaleToScreenWidth'] as int?,
+      crop: map['crop'] as String?,
+      outputFormat: map['outputFormat'] != null 
+          ? IEFormat.values.firstWhere(
+              (e) => e.toString().split('.').last == map['outputFormat'],
+              orElse: () => IEFormat.jpg)
+          : null,
+      format: map['format'] != null 
+          ? IEFormat.values.firstWhere(
+              (e) => e.toString().split('.').last == map['format'],
+              orElse: () => IEFormat.jpg)
+          : null,
+      fitMethod: map['fitMethod'] != null 
+          ? IEFit.values.firstWhere(
+              (e) => e.toString().split('.').last == map['fitMethod'],
+              orElse: () => IEFit.box)
+          : null,
+      fit: map['fit'] != null 
+          ? IEFit.values.firstWhere(
+              (e) => e.toString().split('.').last == map['fit'],
+              orElse: () => IEFit.box)
+          : null,
+      compression: map['compression'] as int?,
+      sharpness: map['sharpness'] as int?,
+      rotate: map['rotate'] as int?,
+      inline: map['inline'] as bool?,
+      keepMeta: map['keepMeta'] as bool?,
+      noOptimization: map['noOptimization'] as bool?,
+      forceDownload: map['forceDownload'] as bool?,
+      maxDevicePixelRatio: map['maxDevicePixelRatio'] as double?,
+    );
+  }
 }
